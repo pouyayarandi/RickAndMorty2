ced53d9488389bd760700d0f61d469bacb5a12bb
Pouya Yarandi <pouya.yarandi@divar.ir>
2023-01-14 12:36:34 +0330

    Initial Commit

---
+++ RickAndMorty2/RickAndMorty2App.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty2App.swift
+//  RickAndMorty2
+//
+//  Created by Pouya Yarandi on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
